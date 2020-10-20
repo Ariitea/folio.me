e2e9b8a15b137d15d6cba87271ad2926898a0430
Adrien Desrames <adriendesrames.ios@gmail.com>
2020-10-21 00:21:13 +0200

    all files

---
+++ main.js
@@ -0,0 +1,32 @@
+//Selectors
+let header = document.querySelector(".header");
+let hamburgerMenu = document.querySelector(".hamburger-menu");
+
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+
+// Hamburger Menu animation
+window.addEventListener('scroll', function(){
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('active', windowPosition)
+});
+
+// Hamburger Menu open function
+hamburgerMenu.addEventListener('click', function() {
+  header.classList.toggle('menu-open')
+});
+
+
+// Cookie fucntion for storage et remove banner
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true")
+});
+
+// Time out storage displayed
+setTimeout(() => {
+  if(!localStorage.getItem("cookieBannerDisplayed"))
+    cookieContainer.classList.add("active")
+}, 2000);
